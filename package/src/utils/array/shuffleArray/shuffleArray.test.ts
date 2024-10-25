bf4e443348e40b751bfb54df655c3a5446713163
doosabhulaxmi <bhulaxmi1999@gmail.com>
2024-10-25 19:36:54 +0530

    changes done

--- package/src/utils/array/shuffleArray/shuffleArray.test.ts
+++ package/src/utils/array/shuffleArray/shuffleArray.test.ts
@@ -3,16 +3,35 @@ import { shuffleArray } from "./shuffleArray";
 describe('shuffleArray', () => {
     it('should shuffle the elements of the array randomly', () => {
         const arr = [1, 2, 3, 4, 5];
-        const shuffledArr = shuffleArray(arr);
+        const iterations = 5000;
+        let shufflesCount = 0;
 
-        expect(shuffledArr).not.toEqual(arr);
-        expect(shuffledArr.length).toBe(arr.length);
-        expect(shuffledArr.sort()).toEqual(arr.sort());
+        for (let i = 0; i < iterations; i++) {
+            const shuffledArr = shuffleArray(arr);
+            if (!arraysEqual(shuffledArr, arr)) {
+                shufflesCount++;
+            }
+        }
+        expect(shufflesCount / iterations).toBeGreaterThanOrEqual(0.98);
 
         expect(shuffleArray([])).toEqual([]);
         expect(shuffleArray([1])).toEqual([1]);
-
     });
+    it('should work with arrays of different data types', () => {
+        const stringArr = ['a', 'b', 'c', 'd', 'e'];
+        const shuffledStringArr = shuffleArray(stringArr);
+
+        expect(shuffledStringArr).not.toEqual(stringArr);
+        expect(shuffledStringArr.length).toBe(stringArr.length);
+        expect(shuffledStringArr.sort()).toEqual(stringArr.sort());
+
+        const objectArr = [{ id: 1 }, { id: 2 }, { id: 3 }];
+        const shuffledObjectArr = shuffleArray(objectArr);
+
+        expect(shuffledObjectArr).not.toEqual(objectArr);
+        expect(shuffledObjectArr.length).toBe(objectArr.length);
+        expect(shuffledObjectArr.map(obj => obj.id).sort()).toEqual(objectArr.map(obj => obj.id).sort());
+    })
 
     it('should return an empty array if input is an empty array', () => {
         const arr: any[] = [];
@@ -28,5 +47,8 @@ describe('shuffleArray', () => {
 
         expect(arr).toEqual(arrCopy);
     });
-    
+    function arraysEqual(a: number[], b: number[]): boolean {
+        return a.length === b.length && a.every((value, index) => value === b[index]);
+    }
+
 });
