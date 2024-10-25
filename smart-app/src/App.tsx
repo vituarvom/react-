7b89b9a113cad12cf36439e2f084f858fe0e3073
snehadomal <snehadomal80@gmail.com>
2024-10-25 17:09:17 +0530

    usage for useClipboard hook

--- smart-app/src/App.tsx
+++ smart-app/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import UseClipboard from "./components/hooks/useClipboard";
 import UseCountDown from "./components/hooks/UseCountDown";
 
 function App() {
   return (
     <>
       <UseCountDown />
+      <UseClipboard />
     </>
   );
 }
