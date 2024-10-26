df808fc82898dbbef6bf84fbb34a1dad8a188106
BethiPooja <bethipooja4@gmail.com>
2024-10-26 17:06:11 +0530

    Changes Done

--- package/src/utils/function/isString/isString.ts
+++ package/src/utils/function/isString/isString.ts
@@ -2,7 +2,3 @@ export function isString(value:any):boolean {
     return typeof value === 'string';
   
 }
-isString('Hello World'); 
-isString(123);           
-isString([]);            
-isString({});            
